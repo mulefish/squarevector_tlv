b168dc58f7c80697ca9a5cf96a850a161e9e6849
mulefish <pmontgomery@lululemon.com>
2024-01-06 11:19:22 -0800

    prettier

--- read_csv.js
+++ read_csv.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require('fs')
 const csv = require('csv-parser');
 const sqlite3 = require('sqlite3').verbose()
 const { importantMsg, COMMON_THINGS } = require("./logic/library.js")
 const { updater, setDB } = require('./logic/db_handler.js')
-const results = [];
+const results = []
 importantMsg("OK", `Use dataBase named '${COMMON_THINGS.DB_NAME}'`)
 setDB("./" + COMMON_THINGS.DB_NAME)
 
@@ -14,7 +14,7 @@ fs.createReadStream('2023-12-13 12_42pm.csv')
         results.forEach(async (obj, i) => {
             const id = obj['OKTA_ID']
             const dollars = parseInt(obj['TOTALREVENUE'])
-            const query = `UPDATE people SET tlv = ${dollars} WHERE pk = "${id}"`;
+            const query = `UPDATE people SET tlv = ${dollars} WHERE pk = "${id}"`
             await updater(query)
             console.log(i, query)
         })
